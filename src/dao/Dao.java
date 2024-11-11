370828edd9e344ba3fb3b9c72cd221ab3fcad79d
riamu <keigokun0419@gmail.com>
2024-11-11 11:21:31 +0900

    11/11 たかの

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,5 +1,35 @@
 package dao;
 
-public class Dao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+
+public class Dao {
+	public static void main(String[] args) {
+		//初期化
+		Connection dbResult = null;
+		try {
+			//個人設定に必要な文字列
+			String dbUrl = "jdbc:postgresql://localhost:5432/test2";
+			String dbUserName = "postgres";
+			String dbPassword = "pass";
+			System.out.println("接続開始。");
+
+			// 接続
+			dbResult = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+			System.out.println("接続できました。");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// クローズ
+				if (dbResult != null) {
+					dbResult.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
