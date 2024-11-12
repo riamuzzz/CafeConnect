7b7202b2a101c53bcb53453187a6c9bd388b6e62
sryotaa <knz2374441@stu.o-hara.ac.jp>
2024-11-12 13:59:18 +0900

    作成

--- src/dao/CardDao.java
+++ src/dao/CardDao.java
@@ -14,11 +14,12 @@ public class CardDao extends Dao {
 	//ゲット
 	public Card get(String cardNumber)throws Exception{
 
+		// Cardをインスタンス化
 		Card card=new Card();
 		Connection connection=getConnection();
 		PreparedStatement statement=null;
 
-		try{   //科目コード別に作成
+		try{
 			statement=connection.prepareStatement(baseSql);
 			statement.setString(1, cardNumber);
 
@@ -29,7 +30,7 @@ public class CardDao extends Dao {
 
 			} else {
 				//リザルトセットが存在しない場合
-				//科目インスタンスにnullをセット
+				//cardインスタンスにnullをセット
 				card= null;
 			} }catch (Exception e) {
 				throw e;
@@ -49,6 +50,76 @@ public class CardDao extends Dao {
 				}
 			}
 
+		// Card型で返す
 		return card;
+	}
+
+	//セーブ（更新＆作成用）
+	public boolean save(Card card) throws Exception {
+		//コネクションを確立
+		Connection connection = getConnection();
+		//プリペアードステートメント
+		PreparedStatement statement = null;
+		//実行件数
+		int count = 0;
+
+		try{
+			//データベースからcardを取得
+			Card old = get(card.getCardNumber());
+			if (old == null) {
+				//card（card_number)が存在しなかった場合
+				//プリペアードステートメンにINSERT文をセット
+				statement = connection.prepareStatement(
+						"insert into card (card_number, card_day, card_cvc, card_name) values(?, ?, ?, ?) ");
+				//プリペアードステートメントに値をバインド
+				statement.setString(1, card.getCardNumber());
+				statement.setString(2,card.getCardExpiryDate());
+				statement.setString(3,card.getCardCvc());
+				statement.setString(4,card.getCardName());
+			} else {
+				//学生が存在した場合
+				//プリペアードステートメントにUPDATE文をセット
+				statement = connection
+						.prepareStatement("update card set card_number=?, card_day=?, card_cvc=?, card_name=? where card_number=?");
+				//プリペアードステートメントに値をバインド
+				statement.setString(1, card.getCardNumber());
+				statement.setString(2,card.getCardExpiryDate());
+				statement.setString(3,card.getCardCvc());
+				statement.setString(4,card.getCardName());
+				statement.setString(5, card.getCardNumber());
+			}
+
+			//プリペアードステートメントを実行
+			count = statement.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//
+			if(statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+
+			if(connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+		if (count > 0) {
+			//実行件数が1以上ある場合
+			return true;
+
+		} else {
+			//実行件数が0件の場合
+			return false;
+		}
+
 	}
 }
--- src/scoremanager/main/CardCreateAction.java
+++ src/scoremanager/main/CardCreateAction.java
@@ -1,5 +1,15 @@
 package scoremanager.main;
 
-public class CardCreateAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import tool.Action;
+
+public class CardCreateAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		req.getRequestDispatcher("card_create.jsp").forward(req, res);
+	}
 }
