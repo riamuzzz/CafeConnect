a9e3c6a2613d15dd62618e7610cf5fbd576a114f
ryotathon <knz2374503@stu.o-hara.ac.jp>
2024-11-12 13:47:09 +0900

    11/12りょたそん

--- src/dao/CafeUserDao.java
+++ src/dao/CafeUserDao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 import bean.CafeUser;
 
-public class CafeUserDao {
+public class CafeUserDao extends Dao {
 	/**
 	 * baseSql:String 共通SQL文 プライベート
 	 */
@@ -15,40 +15,36 @@ public class CafeUserDao {
 
 
 	/**
-	 * getメソッド 教員IDを元に、教員インスタンスを1件取得する
+	 * getメソッド カフェ店員IDを元に、カフェ店員インスタンスを1件取得する
 	 *
-	 * @param id:String 教員ID
-	 * @return 教員クラスのインスタンス 存在しない場合はnull
+	 * @param cafeUserId:String カフェ店員ID
+	 * @return カフェ店員クラスのインスタンス 存在しない場合はnull
 	 * @throws Exception
 	 */
-	public Teacher get(String id) throws Exception{
+
+	public CafeUser get(String cafeUserId) throws Exception{
 		Connection connection = getConnection();
 		//プリペアードステートメント
 		PreparedStatement statement = null;
 		//結果を格納するTeacherを初期化
-		Teacher teacher = new Teacher();
+		CafeUser cafeUser = new CafeUser();
 
 		try{
 			//プリペアードステートメントにSQL文をセット
 			statement = connection.prepareStatement(baseSql);
 
 			//プレースホルダー（？の部分）に値を設定し、SQLを実行
-			statement.setString(1,id);
+			statement.setString(1,cafeUserId);
 			ResultSet rSet = statement.executeQuery();
 
-			//対応するschool型を取得するために使用
-			SchoolDao schoolDao = new SchoolDao();
-
-			//取得した教員ID、パスワード、教員名、学校コードをteacherインスタンスに保存
+			//取得したカフェ店員ID、パスワード、教員名、学校コードをteacherインスタンスに保存
 			//セットするものは[password]じゃなくて「pasword]だよ
 			if(rSet.next()) {
-				teacher.setId(rSet.getString("id"));
-				teacher.setPassword("password");
-				//teacher.setName("name");
-				teacher.setSchool(schoolDao.get("school_cd"));
+				cafeUser.setCafeUserId(rSet.getString("id"));
+				cafeUser.setCafeUserPassword("password");
 			} else {
 				//対応する教員がいない場合はnullを返す
-				teacher = null;
+				cafeUser = null;
 			}
 
 		} catch (Exception e) {
@@ -73,27 +69,27 @@ public class CafeUserDao {
 		}
 	}
 	//検索した教員インスタンスを返す
-	return teacher;
+	return cafeUser;
 	}
 
 	/**
-	 * loginメソッド 教員ID、パスワードを元に、教員インスタンスを1件取得する
+	 * loginメソッド 店員ID、パスワードを元に、店員インスタンスを1件取得する
 	 *
-	 * @param id:String 教員ID
+	 * @param id:String 店員ID
 	 * @param password:String パスワード
-	 * @return 教員クラスのインスタンス 存在しない場合はnull
+	 * @return 店員クラスのインスタンス 存在しない場合はnull
 	 * @throws Exception
 	 */
-	public Teacher login(String id,String password)throws Exception{
+	public CafeUser login(String id,String password)throws Exception{
 		Connection connection = getConnection();
 		//プリペアードステートメント
 		PreparedStatement statement = null;
 
-		//pas  wordだよ
+		//passwordだよ
 		String condition = " and password=?";
 
 		//結果を格納するTeacherを初期化
-		Teacher teacher = new Teacher();
+		CafeUser cafeUser = new CafeUser();
 
 		try{
 			//プリペアードステートメントにSQL文をセット
@@ -104,20 +100,14 @@ public class CafeUserDao {
 			statement.setString(2, password);
 			ResultSet rSet = statement.executeQuery();
 
-			//対応するschool型を取得するために使用
-			SchoolDao schoolDao = new SchoolDao();
-
-
 			//受け取ったものをセットする
-			//取得した教員ID、パスワード、教員名、学校コードをteacherインスタンスに保存
+			//取得した店員ID、パスワードコードをCafeUserインスタンスに保存
 			if(rSet.next()) {
-				teacher.setId(rSet.getString("id"));
-				teacher.setPassword(rSet.getString("password"));
-				teacher.setName(rSet.getString("name"));
-				teacher.setSchool(schoolDao.get(rSet.getString("school_cd")));
+				cafeUser.setCafeUserId(rSet.getString("id"));
+				cafeUser.setCafeUserPassword(rSet.getString("password"));
 			} else {
 				//対応する教員がいない場合はnullを返す
-				teacher = null;
+				cafeUser = null;
 			}
 
 		} catch (Exception e) {
@@ -142,7 +132,7 @@ public class CafeUserDao {
 		}
 	}
 	//検索した教員インスタンスを返す
-	return teacher;
+	return cafeUser;
 
 	}
 
