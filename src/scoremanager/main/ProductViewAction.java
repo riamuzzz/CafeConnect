a93f9d907088726094c0f17857ef3378b593a5ab
1205ki <knz2374470@stu.o-hara.ac.jp>
2024-11-14 10:03:58 +0900

    　　

--- src/scoremanager/main/ProductViewAction.java
+++ src/scoremanager/main/ProductViewAction.java
@@ -1,5 +1,41 @@
 package scoremanager.main;
 
-public class ProductViewAction {
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Category;
+import bean.Product;
+import dao.CategoryDao;
+import dao.ProductDao;
+import tool.Action;
+
+public class ProductViewAction  extends Action {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		String productName = "";
+		String categoryId = null;     // カテゴリ変数 一旦null
+		List<Product> products = null;// productリスト
+
+		System.out.print("01");
+		ProductDao productDao = new ProductDao();
+		System.out.print("02");
+		CategoryDao categoryDao = new CategoryDao();
+		System.out.print("03");
+
+		Category category = categoryDao.get(categoryId);
+
+		System.out.print("04");
+		products = productDao.filter(category, productName);
+
+		System.out.println(products);
+		req.setAttribute("products", products);
+
+
+		System.out.println("1111");
+		req.getRequestDispatcher("top_page.jsp").forward(req, res);
+	}
 
 }
