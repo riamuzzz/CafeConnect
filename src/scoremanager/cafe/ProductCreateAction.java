4987158668bc5153bacbc83fd1d5bbbdcbec9133
sryotaa <knz2374441@stu.o-hara.ac.jp>
2024-11-15 14:59:54 +0900

    カテゴリ名で選択できるようにした

--- src/scoremanager/cafe/ProductCreateAction.java
+++ src/scoremanager/cafe/ProductCreateAction.java
@@ -1,25 +1,26 @@
 package scoremanager.cafe;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Category;
+import dao.CategoryDao;
 import tool.Action;
-
 public class ProductCreateAction extends Action{
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		//ローカル変数の宣言 1
-		//なし
-		//DBからデータ取得 3
-		//なし
-		//ビジネスロジック 4
-		//なし
-		//DBへデータ保存 5
-		//なし
-		//レスポンス値をセット 6
-		//なし
-		//JSPへフォワード 7
+
+		CategoryDao categoryDao = new CategoryDao(); // カテゴリDaoをインスタンス化
+
+		List<Category> categorys = categoryDao.get(); // カテゴリの情報すべて取得
+
+		System.out.println(categorys);
+		req.setAttribute("categorys", categorys); // リクエストパラメータにcategoryをセット
+
+		// 商品登録ページに遷移
 		req.getRequestDispatcher("productCreate.jsp").forward(req, res);
 	}
 }
