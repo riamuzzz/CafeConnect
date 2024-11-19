71752c1e11bc2dc37504214c4f1c19d4427137a8
sryotaa <knz2374441@stu.o-hara.ac.jp>
2024-11-19 15:03:02 +0900

    商品追加できる

--- src/scoremanager/cafe/ProductCreateExecuteAction.java
+++ src/scoremanager/cafe/ProductCreateExecuteAction.java
@@ -1,9 +1,14 @@
 package scoremanager.cafe;
 
+import java.io.File;
+import java.sql.Date;
+import java.time.LocalDate;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Category;
+import bean.Product;
 import dao.CategoryDao;
 import dao.ProductDao;
 import tool.Action;
@@ -18,25 +23,62 @@ public class ProductCreateExecuteAction extends Action{
 		// categoryDaoをインスタンス化
 		CategoryDao categoryDao = new CategoryDao();
 
-		String categoryId = null;     // カテゴリID
-		Category category = null;     // カテゴリ
-		String productName = null;    // 商品名
-		Integer price = null;          // 価格
-		Integer count = null;          // 在庫数
-		String productDetail = null;  // 商品詳細
-		String img = null;             // 写真
-		String sellStr = null;         // 販売状況str型
-		Boolean sell = null;           // 販売状況
+		String productId = "AA12";                   // 商品ID
+		String categoryId = null;                   // カテゴリID
+		Category category = null;                   // カテゴリ
+		String productName = null;                  // 商品名
+		int price = 0;                               // 価格
+		int count = 0;                               // 在庫数
+		String productDetail = null;                // 商品詳細
+		String img = null;                           // 写真
+		String sellStr = null;                       // 販売状況str型
+		Boolean sell = null;                         // 販売状況
+		LocalDate nowDate = LocalDate.now();         // LcalDateインスタンスを取得
+		String nowDateStr = String.valueOf(nowDate); // string型に変換
+		Date inStockDay = Date.valueOf(nowDateStr);  // string型からDate型に変換
 
 		// リクエストパラメータの取得
-		categoryId = req.getParameter("Category"); // カテゴリId
-		productName = req.getParameter("productName"); // 商品名
+		categoryId = req.getParameter("category");           // カテゴリId
+		productName = req.getParameter("productName");       // 商品名
 		price = Integer.parseInt(req.getParameter("price")); // 価格
 		count = Integer.parseInt(req.getParameter("count")); // 在庫数
-		productDetail = req.getParameter("productDetail"); // 商品詳細
-		img = req.getParameter("img"); // 写真
-		sellStr = req.getParameter("sell"); // 販売状況
+		productDetail = req.getParameter("productDetail");   // 商品詳細
+		img = req.getParameter("image");                     // 写真
+		sellStr = req.getParameter("sell");                  // 販売状況
 
+		System.out.print("sellStr:");
+		System.out.println(sellStr);
+		if (sellStr != "on"){
+			sell = false;
+		} else {
+			sell = true;
+		}
+
+		File file = new File("/CafeConnect/WebContent/scoremanager/img/product/a.jpg");
+		System.out.println(file);
+
+		System.out.println(categoryId);
+		category = categoryDao.get(categoryId);
+
+		// productを初期化
+		Product product = new Product();
+
+		// productに値をset
+		product.setProductId(productId);
+		product.setCategory(category);
+		product.setProductName(productName);
+		product.setPrice(price);
+		product.setCount(count);
+		product.setProductDetail(productDetail);
+		product.setImage(img);
+		product.setSell(sell);
+		product.setInStockDay(inStockDay);
+
+		// productをsave
+		productDao.save(product);
+
+		// 商品登録ページに遷移
+		req.getRequestDispatcher("productCreate.jsp").forward(req, res);
 
 	}
 }
